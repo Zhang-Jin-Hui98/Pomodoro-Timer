1eb10a52a02c9e595fca7581c485a517c675a8de
Zhang-Jin-Hui98 <113121141@gms.tcu.edu.tw>
2026-05-20 16:04:35 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base:"/Pomodoro-Timer/"
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
