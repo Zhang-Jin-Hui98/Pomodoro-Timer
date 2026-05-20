fee972047b0886ba142c92a4f485f0c361f1a74c
Zhang-Jin-Hui98 <113121141@gms.tcu.edu.tw>
2026-05-20 16:10:12 +0800

    Fix formatting in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base:"/Pomodoro-Timer/"
+    base:"/Pomodoro-Timer/",
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
